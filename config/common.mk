LiquidSmooth-Old/android_vendor_liquid

Xposed backup script
Save xposed intallation when flashing nightlies

Change-Id: Id5f27fbc34ec0fed7fa00db35757a708a287df34

config/common.mk
@@ -101,6 +101,7 @@ PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/liquid/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/liquid/prebuilt/common/bin/50-liquid.sh:system/addon.d/50-liquid.sh \
+    vendor/liquid/prebuilt/common/bin/90-xposed.sh:system/addon.d/90-xposed.sh \
     vendor/liquid/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
 # bin

SeniorLimpio/vendor_ldroid

Conflicts:
	config/common.mk

config/common.mk
@@ -42,6 +42,7 @@ PRODUCT_COPY_FILES += \
     vendor/ldroid/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/ldroid/prebuilt/common/bin/50-ldroid.sh:system/addon.d/50-ldroid.sh \
     vendor/ldroid/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/ldroid/prebuilt/common/bin/90-xposed.sh:system/addon.d/90-xposed.sh \
     vendor/ldroid/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # L-Droid-specific init file